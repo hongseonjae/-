3febd47706e831b247ecef9de9010cd9e1cbb61f
횽셙9 <71117046+hongseonjae@users.noreply.github.com>
2020-09-14 23:38:41 +0900

    Create 05_practice_2.ino
    창연공 2주차 실습과제 제출물입니다.
    (2020/09/14 - 23: 38)

---
+++ 05_practice_2.ino
@@ -0,0 +1,29 @@
+void setup()
+{
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  int i = 1;
+
+  digitalWrite(7,LOW);
+  delay(1000);
+  digitalWrite(7,HIGH);
+  delay(1000);
+  
+  while(i<=5) {  
+    digitalWrite(7, LOW); //LED ON
+    delay(500);  
+    
+    digitalWrite(7, HIGH);  //LED OFF
+    delay(500); 
+   
+    i++;  
+   } 
+  while(Serial){
+    digitalWrite(7,HIGH);
+  }
+  
+  
+}
